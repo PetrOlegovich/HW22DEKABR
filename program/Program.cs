d15f84ea8ca0f886f94eceb8d1f42606a293e6df
PETR_OLEGOVICH <Kartmansp@mail.ru>
2022-12-22 20:44:41 +0300

    создание цикла проверки всех слов на количество символов

--- program/Program.cs
+++ program/Program.cs
@@ -1,3 +1,11 @@
 ﻿Console.WriteLine("введите текст через один пробел. без запятых)))");
 string text = Console.ReadLine();
 string [] littleWords = text.Split(' ');
+
+for (int i = 0; i < littleWords.Length; i++)
+{
+    if (littleWords[i].Length <= 3 )
+    {
+        Console.Write($"[{littleWords[i]}] ");
+    }
+}
